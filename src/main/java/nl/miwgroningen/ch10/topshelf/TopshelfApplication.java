0c9dc566d23c541f5e59b1dec15d47afdf5924ce
JackWieringa <jackwieringa@hotmail.com>
2023-01-26 11:08:06 +0100

    Springboot application POM updated

---
+++ src/main/java/nl/miwgroningen/ch10/topshelf/TopshelfApplication.java
@@ -0,0 +1,13 @@
+package nl.miwgroningen.ch10.topshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopshelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopshelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwgroningen/ch10/topshelf/TopshelfApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwgroningen.ch10.topshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopshelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
