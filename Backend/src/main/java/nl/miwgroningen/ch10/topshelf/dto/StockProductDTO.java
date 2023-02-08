d62cb1907acbd2928dae9619a9163586888451c1
IPostma <inge.dikland@gmail.com>
2023-02-08 12:10:34 +0100

    TS-49 DTO's implemented

---
+++ Backend/src/main/java/nl/miwgroningen/ch10/topshelf/dto/StockProductDTO.java
@@ -0,0 +1,7 @@
+package nl.miwgroningen.ch10.topshelf.dto;/** 
+ * @author Inge Dikland
+ *
+ * Dit is wat het programma doet.
+ */
+public class StockProductDTO {
+}
---
+++ Backend/src/main/java/nl/miwgroningen/ch10/topshelf/mapper/ProductDefinitionDTOMapper.java
@@ -0,0 +1,7 @@
+package nl.miwgroningen.ch10.topshelf.mapper;/** 
+ * @author Inge Dikland
+ *
+ * Dit is wat het programma doet.
+ */
+public class ProductDefinitionDTOMapper {
+}
