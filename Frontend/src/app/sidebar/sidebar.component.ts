bd99c678c48ec71ed20b7d2d855028ee5c85334e
JackWieringa <118735373+JackWieringa@users.noreply.github.com>
2023-03-13 20:26:38 +0100

    TS-118 Fixed issue

--- Frontend/src/app/sidebar/sidebar.component.ts
+++ Frontend/src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterModule, Routes } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule, Routes } from '@angular/router';
+import { Pantry } from '../pantry/pantry';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,7 +9,9 @@ import { ActivatedRoute, RouterModule, Routes } from '@angular/router';
 })
 export class SidebarComponent implements OnInit {
   pantryId?: number;
+  initialPantryId?: number;
   pantryName?: string;
+  pantry!: Pantry;
 
   constructor(private route: ActivatedRoute) {}
 
@@ -16,13 +19,19 @@ export class SidebarComponent implements OnInit {
     this.route.parent?.params.subscribe((params) => {
       this.pantryId = params['pantryId'];
     });
-
     this.getPantryName();
+    this.getPantryId();
   }
 
   public getPantryName() {
     this.route.queryParams.subscribe((params) => {
       this.pantryName = params['name'];
+    });
+  }
+
+  public getPantryId() {
+    this.route.queryParams.subscribe((params) => {
+      this.initialPantryId = params['id'];
     });
   }
 }
