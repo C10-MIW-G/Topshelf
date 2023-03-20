c6a031b60b22b09c538700e4ec6f0882a5112a70
Jacob Visser <jacobvisser123@hotmail.com>
2023-03-20 11:25:48 +0100

    Logo adjusted homepage.

--- Frontend/src/app/nav/nav.component.ts
+++ Frontend/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 import { TokenStorageService } from '../_services/token-storage.service';
 
 @Component({
@@ -6,8 +7,28 @@ import { TokenStorageService } from '../_services/token-storage.service';
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css'],
 })
-export class NavComponent {
-  constructor(private tokenStorageService: TokenStorageService) {}
+export class NavComponent implements OnInit {
+  currentUrl: string;
+  atHome: boolean = false;
+
+  constructor(
+    private tokenStorageService: TokenStorageService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.currentUrl = window.location.href;
+  }
+  ngOnInit(): void {
+    this.isAtHome();
+    console.log(this.currentUrl);
+    console.log(this.atHome);
+  }
+
+  public isAtHome(): void {
+    if (this.currentUrl === 'http://localhost:4200/home') {
+      this.atHome = true;
+    }
+  }
 
   public isLoggedIn(): boolean {
     return this.tokenStorageService.isLoggedIn();
